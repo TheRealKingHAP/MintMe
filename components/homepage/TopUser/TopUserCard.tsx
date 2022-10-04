292ff04e1a72d7b7016096cabf1af83e18c4d1eb
KingHAP <aviapech@hotmail.com>
2022-10-04 17:45:05 -0500

    TopUserCard image zoom animation implementation

--- components/homepage/TopUser/TopUserCard.tsx
+++ components/homepage/TopUser/TopUserCard.tsx
@@ -13,7 +13,7 @@ function TopUserCard({username, img, mainplatform, introduction}: UserCardProps)
   return (
     <div className='flex flex-col space-y-2  items-center text-center 2xl:items-start 2xl:text-left  w-52 h-max cursor-pointer'>
         <div className='relative w-full h-48 bg-gray-100'>
-            <Image src={img} layout='fill' objectFit='contain' />
+            <Image src={img} layout='fill' objectFit='contain' className='transition-none 2xl:hover:scale-125  2xl:transition 2xl:ease-in-out 2xl:duration-700' />
         </div>
         <div className='h-16'>
         <p className='font-normal text-gray-500 line-clamp-2'> <strong className='text-gray-800'>{username}</strong> {introduction}</p>
